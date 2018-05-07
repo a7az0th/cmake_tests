ce3e1470581b3b5a2e84c95a9ffcc84be37770c4
Alexander Soklev <a7az0th@gmail.com>
2018-05-07 15:28:19 +0300

    Basic gradient implementation. Added color header

---
+++ opengl/color.h
@@ -0,0 +1,102 @@
+#pragma once
+
+typedef unsigned char  uint8;
+
+inline float min(float a, float b) { return a < b ? a : b; }
+inline float max(float a, float b) { return a > b ? a : b; }
+inline float clamp(float val, float minVal, float maxVal) { return min(max(val, minVal), maxVal); }
+
+struct Color {
+
+	float r, g, b;
+	Color() {}
+	Color(float r, float g, float b): r(r), b(b), g(g) {}
+
+	Color operator * (float scalar) const {
+		return Color(r * scalar, g * scalar, b * scalar);
+	}
+
+	Color operator * (const Color& rhs) const {
+		return Color(r * rhs.r, g * rhs.g, b * rhs.b);
+	}
+
+	Color& operator += (float scalar) {
+		r += scalar;
+		g += scalar;
+		b += scalar;
+		return *this;
+	}
+
+	Color& Color::operator += (const Color& rhs) {
+		r += rhs.r;
+		g += rhs.g;
+		b += rhs.b;
+		return *this;
+	}
+
+	Color& Color::operator -= (float scalar) {
+		r -= scalar;
+		g -= scalar;
+		b -= scalar;
+		return *this;
+	}
+
+	Color& Color::operator -= (const Color& rhs) {
+		r -= rhs.r;
+		g -= rhs.g;
+		b -= rhs.b;
+		return *this;
+	}
+
+	Color& Color::operator /= (float scalar) {
+		scalar = 1.0f/scalar;
+		r *= scalar;
+		g *= scalar;
+		b *= scalar;
+		return *this;
+	}
+
+	Color& operator=(const Color& rhs) {
+		r = rhs.r;
+		g = rhs.g;
+		b = rhs.b;
+		return *this;
+	}
+
+	Color(const Color& rhs) {
+		r = rhs.r;
+		g = rhs.g;
+		b = rhs.b;
+	}
+
+	Color operator / (float scalar) const {
+		scalar = 1.0f / scalar;
+		return Color(r * scalar, g * scalar, b * scalar);
+	}
+
+	Color operator - (const Color& rhs) const {
+		return Color(r - rhs.r, g - rhs.g, b - rhs.b);
+	}
+
+	Color operator + (const Color& rhs) const {
+		return Color(r + rhs.r, g + rhs.g, b + rhs.b);
+	}
+
+	Color operator - () const {
+		return Color(-r, -g, -b);
+	}
+
+	uint8 getRedUINT8()   const { return uint8(clamp(r,0.0f,1.0f)*255.0f); }
+	uint8 getGreenUINT8() const { return uint8(clamp(g,0.0f,1.0f)*255.0f); }
+	uint8 getBlueUINT8()  const { return uint8(clamp(b,0.0f,1.0f)*255.0f); }
+
+	float intensity() const { return (r+g+b)/3.0f; }
+
+	void makeZero() {
+		r = g = b = 0.0f;
+	}
+};
+
+//inline Color operator*(const Color &a, const Color &b) {
+//	return Color(a.r*b.r, a.b*b.b, a.g*b.g);
+//}
--- opengl/main.cpp
+++ opengl/main.cpp
@@ -16,33 +16,57 @@
 	#endif //WIN32
 #endif //APPLE
 
+#include "color.h"
 
+const int width = 320;
+const int height = 320;
+
+Color buffer[width*height];
+
+typedef unsigned int uint32;
+
+void doStuff(Color* image) {
+
+	Color R(1.f, 0.f, 0.f);
+	Color G(0.f, 1.f, 0.f);
+	Color B(0.f, 0.f, 1.f);
+	Color C(0.f, 1.f, 1.f);
+
+
+	for (int y = 0; y < height; y++) {
+		const float k = y / float(height-1);
+
+		Color P = R*k + C*(1-k);
+		Color Q = B*k + G*(1-k);
+
+		for (int x = 0; x < width; x++) {
+			const float k = x / float(width-1);
+			image[y*width + x] = P*k + Q*(1-k);
+		}
+	}
+}
 
 void display() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
- 
-   // Draw a Red 1x1 Square centered at origin
-   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-      glColor3f(1.0f, 0.0f, 0.0f); // Red
-      glVertex2f(-0.5f, -0.5f);    // x, y
-      glVertex2f( 0.5f, -0.5f);
-      glVertex2f( 0.5f,  0.5f);
-      glVertex2f(-0.5f,  0.5f);
-   glEnd();
- 
-   glFlush();  // Render now
+
+	doStuff(buffer);
+
+	glDrawPixels(width, height, GL_RGB, GL_FLOAT ,(float*)buffer);
+	glutSwapBuffers();
+
+	//glutPostRedisplay();
+	glFlush();
 }
 
 
 int main(int argc, char ** argv) {
 
-   glutInit(&argc, argv);                 // Initialize GLUT
-   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
-   glutInitWindowSize(320, 320);   // Set the window's initial width & height
-   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-   glutDisplayFunc(display); // Register display callback handler for window re-paint
-   glutMainLoop();
+	glutInit(&argc, argv);                 // Initialize GLUT
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(width, height);   // Set the window's initial width & height
+	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+	glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+	glutDisplayFunc(display); // Register display callback handler for window re-paint
+	glutMainLoop();
 
 	return 0;
 }
